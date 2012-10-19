bb93ff3c6b88a351abbfac17ea48416eed702d6c
ningchunlei <ningchunlei@gmail.com>
2012-10-19 15:06:36 +0800

    add bin

---
+++ thrift-src/mrelation.thrift
@@ -0,0 +1,15 @@
+namespace java org.iko.thrift
+
+include "ShareStruct.thrift"
+include "Exception.thrift"
+
+service MRelationIFace{
+       map<string,i32> getMsgCounter(1: list<string> mids) throws (1:Exception.EngineException error),
+       list<string> getRelatedMsg(1: string mid,2: i32 start,3: i32 len) throws (1:Exception.EngineException error),
+       i32 initMsg(1:string mid) throws (1:Exception.EngineException error),
+       i32 incr(1:string mid) throws (1:Exception.EngineException error),
+       i32 decr(1:string mid) throws (1:Exception.EngineException error),
+       i32 addRelatedMsg(1:string mid,2:string answer) throws (1:Exception.EngineException error)
+       i32 deleteRelatedMsg(1:string mid,2:string answer) throws (1:Exception.EngineException error)
+}
+
