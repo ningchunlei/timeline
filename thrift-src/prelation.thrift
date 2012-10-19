f96bf6ff8a59430759db25b0fbac3b61b775b16c
ningchunlei <ningchunlei@gmail.com>
2012-10-19 10:36:38 +0800

    add bin

--- thrift-src/prelation.thrift
+++ thrift-src/prelation.thrift
@@ -7,8 +7,8 @@ service PRelationIFace{
        i32 addFollow(1: string uid,2: string followId,3:ShareStruct.RelationType type) throws (1:Exception.EngineException error),
        i32 isFollow(1:string uid,2:string followId,3:ShareStruct.RelationType type) throws (1:Exception.EngineException error),
        i32 cancelFollow(1: string uid,2: string followId,3:ShareStruct.RelationType type) throws (1:Exception.EngineException error),
-       i32 addFans(1: string uid,2: string fansId) throws (1:Exception.EngineException error),
-       list<string> getFollow(1:string uid,2:i32 start,3:i32 len) throws (1:Exception.EngineException error),
-       list<string> getFans(1:string uid,2:i32 start,3:i32 len) throws (1:Exception.EngineException error)
+       i32 addFans(1: string uid,2: string fansId,3:ShareStruct.RelationType type) throws (1:Exception.EngineException error),
+       list<string> getFollow(1:string uid,2:i32 start,3:i32 len,4 :ShareStruct.RelationType type) throws (1:Exception.EngineException error),
+       list<string> getFans(1:string uid,2:i32 start,3:i32 len,4:ShareStruct.RelationType type) throws (1:Exception.EngineException error)
 }
 
